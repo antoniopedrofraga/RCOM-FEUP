6fc0b7e7f56281165f37deec14b1d094fa32da93
root <root@gnu21.netlab.fe.up.pt>
2015-12-15 23:57:27 +0000

    tux4.sh updated

--- proj2/scripts/tux4.sh
+++ proj2/scripts/tux4.sh
@@ -3,5 +3,7 @@
 ifconfig up eth0 172.16.10.254/24
 ifconfig up eth1 172.16.11.253/24
 route add default gw 172.16.11.254
+echo 1 > /proc/sys/net/ipv4/ip_forward
+echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
 printf "search lixa.fe.up.pt\nnameserver 172.16.1.1\n" > /etc/resolv.conf
 echo "tux4 configured"
