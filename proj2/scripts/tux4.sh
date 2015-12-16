4a08236cb5bc98f785b029aebe430b8831f67d3e
root <root@gnu21.netlab.fe.up.pt>
2015-12-16 01:45:19 +0000

    sh updated

--- proj2/scripts/tux4.sh
+++ proj2/scripts/tux4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ifconfig up eth0 172.16.10.254/24
-ifconfig up eth1 172.16.11.253/24
+ifconfig eth0 up 172.16.10.254/24
+ifconfig eth1 up 172.16.11.253/24
 route add default gw 172.16.11.254
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
